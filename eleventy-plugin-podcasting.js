eeeb8ae5fb0945f1c9d4fb53d90a866455723c7e
Nathan Bottomley <nathan.bottomley@gmail.com>
2024-08-03 19:53:25 +1000

    Moved feed out of plugin file; added tests

--- eleventy-plugin-podcasting.js
+++ eleventy-plugin-podcasting.js
@@ -1,69 +1,7 @@
 import { DateTime } from 'luxon'
 import rssPlugin from '@11ty/eleventy-plugin-rss'
+import { readFileSync } from 'node:fs'
 
-const podcastFeedTemplate = `
-<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0" 
-  xmlns:atom="http://www.w3.org/2005/Atom"
-  xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" 
-  xmlns:content="http://purl.org/rss/1.0/modules/content/"
-  >
-  <channel>
-    <title>{{ podcast.title }}</title>
-    <itunes:subtitle>{{ podcast.subtitle }}</itunes:subtitle>
-    <description>{{ podcast.description }}</description>
-    <link>{{ podcast.siteUrl }}</link>
-    <atom:link href="{{ permalink | htmlBaseUrl(podcast.siteUrl) }}" rel="self" type="application/rss+xml" />
-    <itunes:owner>
-      <itunes:name>{{ podcast.owner.name }}</itunes:name>
-      <itunes:email>{{ podcast.owner.email }}</itunes:email>
-    </itunes:owner>
-    <itunes:author>{{ podcast.author }}</itunes:author>
-    <itunes:category text="{{ podcast.category }}">
-      <itunes:category text="{{ podcast.subcategory }}" />
-    </itunes:category>
-    <itunes:image href="{{ podcast.imagePath | htmlBaseUrl(podcast.siteUrl) }}"></itunes:image>
-    <itunes:summary>{{ podcast.summary }}</itunes:summary>
-    <itunes:explicit>{{ podcast.explicit or "no" }}</itunes:explicit>
-    <itunes:type>{{ podcast.type or "episodic" }}</itunes:type>
-    <itunes:complete>{{ podcast.complete or "no" }}</itunes:complete>
-    <language>{{ podcast.language }}</language>
-    <copyright>{{ podcast.copyrightNotice }}</copyright>
-    <pubDate>{{ collections.post | getNewestCollectionItemDate | dateToRfc3339 }}</pubDate>
-    <lastBuildDate>{% feedLastBuildDate %}</lastBuildDate>
-    <generator>{{ eleventy.generator }}</generator>
-    {% asyncEach post in collections.post | reverse %}
-    <item>
-      <title>{{ post.data.title }}</title>
-      <link>{{ post.url | htmlBaseUrl(podcast.siteUrl) }}</link>
-      <pubDate>{{ post.date | dateToRfc3339 }}</pubDate>
-      {% if post.data.seasonNumber -%}
-      <itunes:season>{{ post.data.seasonNumber }}</itunes:season>
-      {%- endif %}
-      <itunes:episode>{{ post.data.episodeNumber }}</itunes:episode>
-      <itunes:summary>{{ post.content | striptags(true) | truncate(800) }}</itunes:summary>
-      <description>{{ post.content | striptags(true) | truncate(800) }}</description>
-      <content:encoded>
-        <![CDATA[{% include "feed-episode.njk" -%}]]>
-      </content:encoded>
-      <enclosure url="{{ post.data.episodeUrl }}" length="{{ post.data.episodeSize }}" type="audio/mp3"></enclosure>
-      <itunes:duration>{{ post.data.episodeDuration }}</itunes:duration>
-      {%- if post.data.guid != undefined %}
-      <guid isPermalink="false">{{ post.data.guid }}</guid>
-      {% else %}
-      <guid isPermalink="true">{{ post.url | htmlBaseUrl(podcast.siteUrl) }}</guid>
-      {% endif -%}
-      {%- if post.data.explicit != undefined %}
-      <itunes:explicit>{{ post.data.explicit }}</itunes:explicit>
-      {% endif -%}
-      {%- if post.data.episodeType != undefined %}
-      <itunes:episodeType>{{ post.data.episodeType }}</itunes:episodeType>
-      {% endif -%}
-    </item>
-    {% endeach -%}
-  </channel>
-</rss>
-`
 export default function (eleventyConfig) {
   if (!('addTemplate' in eleventyConfig)) {
     console.log('[eleventy-plugin-podcasting] WARN Eleventy plugin compatibility: Virtual Templates are required for this plugin — please use Eleventy v3.0 or newer.')
@@ -74,6 +12,7 @@ export default function (eleventyConfig) {
       closingSingleTag: 'default' // opt-out of <img/>-style XHTML single tags
     }
   })
+
   eleventyConfig.addGlobalData('eleventyComputed.podcast.copyrightNotice', () => {
     return data => {
       const thisYear = DateTime.now().year
@@ -90,12 +29,14 @@ export default function (eleventyConfig) {
   eleventyConfig.addShortcode('feedLastBuildDate', () =>
     DateTime.now().toRFC2822()
   )
+
   eleventyConfig.addGlobalData('eleventyComputed.episodeUrl', function (filename) {
     return data => {
       const episodePrefix = data.podcast.episodePrefix
       return encodeURI(`${episodePrefix}${data.episodeFilename}`)
     }
   })
+
   eleventyConfig.addShortcode('year', () => DateTime.now().year)
   eleventyConfig.addFilter('readableDate', function (date) {
     if (date instanceof Date) {
@@ -106,7 +47,8 @@ export default function (eleventyConfig) {
     })
     return result.setLocale('en-GB').toLocaleString(DateTime.DATE_HUGE)
   })
-  eleventyConfig.addTemplate('feed.njk', podcastFeedTemplate, {
+
+  eleventyConfig.addTemplate('feed.njk', readFileSync('./src/podcastFeed.njk'), {
     permalink: '/feed/podcast',
     eleventyExcludeFromCollections: true,
     eleventyImport: {
--- test/feedTests.js
+++ test/feedTests.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import Eleventy from '@11ty/eleventy'
 import podcastingPlugin from 'eleventy-plugin-podcasting'
-import { XMLParser } from 'fast-xml-parser'
+import { XMLParser, XMLValidator } from 'fast-xml-parser'
 
 test('feed tests run', t => {
   t.pass()
@@ -14,7 +14,45 @@ test('RSS feed template renders', async t => {
     }
   })
   const build = await eleventy.toJSON()
-  t.like(build, [{ url: '/feed/podcast' }])
+  const feed = build[0].content
+  t.true(XMLValidator.validate(feed))
+})
+
+test('RSS feed contains correct information', async t => {
+  const eleventy = new Eleventy('./test', './test/_site', {
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcastingPlugin)
+      eleventyConfig.addGlobalData('podcast', {
+        title: 'Test Podcast',
+        subtitle: 'A test podcast. With cake.',
+        description: 'A test podcast with excellent content. With cake.'
+      })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const parser = new XMLParser()
+  const feedData = parser.parse(build[0].content)
+  t.like(feedData, {
+    rss: {
+      channel: {
+        title: 'Test Podcast',
+        'itunes:subtitle': 'A test podcast. With cake.',
+        description: 'A test podcast with excellent content. With cake.'
+      }
+    }
+  })
+})
+
+test('RSS feed is valid XML', async t => {
+  const eleventy = new Eleventy('./test', './test/_site', {
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcastingPlugin)
+    }
+  })
+  const build = await eleventy.toJSON()
+  const parser = new XMLParser()
+  const feedData = parser.parse(build[0].content)
+  t.truthy(feedData)
 })
 
 test('<itunes:type> defaults to episodic', async t => {
@@ -40,4 +78,42 @@ test('<itunes:type> can be set to serial', async t => {
   const parser = new XMLParser()
   const feedData = parser.parse(build[0].content)
   t.like(feedData, { rss: { channel: { 'itunes:type': 'serial' } } })
+})
+
+test('<itunes:explicit> defaults to not existing', async t => {
+  const eleventy = new Eleventy('./test', './test/_site', {
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcastingPlugin)
+    }
+  })
+  const build = await eleventy.toJSON()
+  const parser = new XMLParser()
+  const feedData = parser.parse(build[0].content)
+  t.false('itunes:explicit' in feedData.rss.channel)
+})
+
+test('<itunes:explicit> can be set to true', async t => {
+  const eleventy = new Eleventy('./test', './test/_site', {
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcastingPlugin)
+      eleventyConfig.addGlobalData('podcast', { explicit: true })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const parser = new XMLParser()
+  const feedData = parser.parse(build[0].content)
+  t.like(feedData, { rss: { channel: { 'itunes:explicit': true } } })
+})
+
+test('<itunes:explicit> can be set to false', async t => {
+  const eleventy = new Eleventy('./test', './test/_site', {
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcastingPlugin)
+      eleventyConfig.addGlobalData('podcast', { explicit: false })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const parser = new XMLParser()
+  const feedData = parser.parse(build[0].content)
+  t.like(feedData, { rss: { channel: { 'itunes:explicit': false } } })
 })

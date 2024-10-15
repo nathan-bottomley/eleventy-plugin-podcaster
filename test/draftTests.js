0130f648fc90d79672c8b41314c861f241cb5c4e
Nathan Bottomley <nathan.bottomley@gmail.com>
2024-10-15 20:41:19 +1100

    Added some drafts tests

--- test/draftTests.js
+++ test/draftTests.js
@@ -1,6 +1,117 @@
 import test from 'ava'
 import Eleventy from '@11ty/eleventy'
+import podcasterPlugin from 'eleventy-plugin-podcaster'
 
-test('podcast data tests run', t => {
-  t.pass()
+// These tests need to be run serially because they rely on setting and unsetting environment variables.
+
+test.serial('drafts status is ignored if plugin added without handleDrafts option', async t => {
+  const eleventy = new Eleventy('./test', './test/_site', {
+    configPath: null,
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcasterPlugin)
+      eleventyConfig.addGlobalData('podcast.episodeUrlBase', 'https://example.com/')
+      eleventyConfig.addTemplate('episode-1.md', '# Episode 1\n\n> blockquote paragraph\n\nnon-blockquote paragraph', {
+        tags: ['podcastEpisode'],
+        date: '2020-01-01',
+        title: 'Episode 1',
+        permalink: '/1/',
+        episode: { filename: 'episode-1.mp3' },
+        draft: true
+      })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const item = build.find(item => item.url === '/1/')
+  t.false(item === undefined)
+})
+
+test.serial('drafts are built if BUILD_DRAFTS is absent and ELEVENTY_RUN_MODE is watch', async t => {
+  process.env.BUILD_DRAFTS = ''
+  const eleventy = new Eleventy('./test', './test/_site', {
+    configPath: null,
+    runMode: 'watch',
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcasterPlugin, { handleDrafts: true })
+      eleventyConfig.addGlobalData('podcast.episodeUrlBase', 'https://example.com/')
+      eleventyConfig.addTemplate('episode-1.md', '# Episode 1\n\n> blockquote paragraph\n\nnon-blockquote paragraph', {
+        tags: ['podcastEpisode'],
+        date: '2020-01-01',
+        title: 'Episode 1',
+        permalink: '/1/',
+        episode: { filename: 'episode-1.mp3' },
+        draft: true
+      })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const item = build.find(item => item.url === '/1/')
+  t.false(item === undefined)
+})
+
+test.serial('drafts are not built if BUILD_DRAFTS is absent and ELEVENTY_RUN_MODE is build', async t => {
+  process.env.BUILD_DRAFTS = ''
+  const eleventy = new Eleventy('./test', './test/_site', {
+    configPath: null,
+    runMode: 'build',
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcasterPlugin, { handleDrafts: true })
+      eleventyConfig.addGlobalData('podcast.episodeUrlBase', 'https://example.com/')
+      eleventyConfig.addTemplate('episode-1.md', '# Episode 1\n\n> blockquote paragraph\n\nnon-blockquote paragraph', {
+        tags: ['podcastEpisode'],
+        date: '2020-01-01',
+        title: 'Episode 1',
+        permalink: '/1/',
+        episode: { filename: 'episode-1.mp3' },
+        draft: true
+      })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const item = build.find(item => item.url === '/1/')
+  t.true(item === undefined)
+})
+
+
+test.serial('drafts are not built if plugin added with handleDrafts option and BUILD_DRAFTS is false', async t => {
+  process.env.BUILD_DRAFTS = 'false'
+  const eleventy = new Eleventy('./test', './test/_site', {
+    configPath: null,
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcasterPlugin, { handleDrafts: true })
+      eleventyConfig.addGlobalData('podcast.episodeUrlBase', 'https://example.com/')
+      eleventyConfig.addTemplate('episode-1.md', '# Episode 1\n\n> blockquote paragraph\n\nnon-blockquote paragraph', {
+        tags: ['podcastEpisode'],
+        date: '2020-01-01',
+        title: 'Episode 1',
+        permalink: '/1/',
+        episode: { filename: 'episode-1.mp3' },
+        draft: true
+      })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const item = build.find(item => item.url === '/1/')
+  t.true(item === undefined)
+})
+
+test.serial('drafts are built if plugin added with handleDrafts option and BUILD_DRAFTS is true', async t => {
+  process.env.BUILD_DRAFTS = 'true'
+  const eleventy = new Eleventy('./test', './test/_site', {
+    configPath: null,
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcasterPlugin, { handleDrafts: true })
+      eleventyConfig.addGlobalData('podcast.episodeUrlBase', 'https://example.com/')
+      eleventyConfig.addTemplate('episode-1.md', '# Episode 1\n\n> blockquote paragraph\n\nnon-blockquote paragraph', {
+        tags: ['podcastEpisode'],
+        date: '2020-01-01',
+        title: 'Episode 1',
+        permalink: '/1/',
+        episode: { filename: 'episode-1.mp3' },
+        draft: true
+      })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const item = build.find(item => item.url === '/1/')
+  t.false(item === undefined)
 })

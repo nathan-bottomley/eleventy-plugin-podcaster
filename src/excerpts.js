4f4bffba17ed45847897f02483f9a348c31c69d2
Nathan Bottomley <nathan.bottomley@gmail.com>
2024-10-23 11:03:23 +1100

    Implemented excerpts with comment delimiters; added test

--- src/excerpts.js
+++ src/excerpts.js
@@ -10,14 +10,30 @@ export default function (eleventyConfig, options = {}) {
       if (!data.tags?.includes('podcastEpisode')) return
 
       const md = markdownIt()
+
+      // If an excerpt is set in front matter, use it
       if (data.excerpt) {
         return md.render(data.excerpt)
       }
-      let content = data.page.rawInput
-      if (data.page.templateSyntax.includes('md')) {
-        content = md.render(content)
+
+      const content = data.page.rawInput
+      const contentIsMarkdown = data.page.templateSyntax.includes('md')
+
+      // If an excerpt is set using comment delimiters, use it
+      const excerptPattern = /<!---excerpt-->\s*(.*?)\s*<!---endexcerpt-->/
+      const match = excerptPattern.exec(content)
+      if (match && contentIsMarkdown) {
+        return md.render(match[1])
+      } else if (match) {
+        return match[1]
       }
-      const dom = htmlparser2.parseDocument(content)
+
+      // If an excerpt is not set, use the first paragraph not in a blockquote
+      let htmlContent
+      if (data.page.templateSyntax.includes('md')) {
+        htmlContent = md.render(content)
+      } // otherwise it's already HTML
+      const dom = htmlparser2.parseDocument(htmlContent)
       const paragraph = dom.children.find(item => item.type === 'tag' && item.name === 'p')
       if (paragraph) {
         return render(paragraph)
--- test/excerptTests.js
+++ test/excerptTests.js
@@ -47,3 +47,24 @@ test('if front matter excerpt is set, it is used after being converted to raw HT
   const item = build.find(item => item.url === '/excerpts/')
   t.true(item.content.includes('<p>front matter <strong>excerpt</strong></p>'), `Excerpt not included in '${item.content}'`)
 })
+
+test('if excerpt is set using comment delimiters, it is used after being converted to raw HTML', async t => {
+  const eleventy = new Eleventy('./test', './test/_site', {
+    configPath: null,
+    config (eleventyConfig) {
+      eleventyConfig.addPlugin(podcasterPlugin, { handleExcerpts: true })
+      eleventyConfig.addGlobalData('podcast.episodeUrlBase', 'https://example.com/')
+      eleventyConfig.addTemplate('episode-1.md', '# Episode 1\n\nfirst paragraph\n<!---excerpt-->\nsecond *paragraph*\n<!---endexcerpt-->\nthird paragraph\n', {
+        tags: ['podcastEpisode'],
+        date: '2020-01-01',
+        title: 'Episode 1',
+        permalink: '/1/',
+        episode: { filename: 'episode-1.mp3' }
+      })
+      eleventyConfig.addTemplate('index.md', '# Excerpts\n\n{% for post in collections.podcastEpisode %}Excerpt is {{ post.data.excerpt }}{% endfor %}', { date: '2020-01-01', title: 'Excerpts', permalink: '/excerpts/' })
+    }
+  })
+  const build = await eleventy.toJSON()
+  const item = build.find(item => item.url === '/excerpts/')
+  t.true(item.content.includes('<p>second <em>paragraph</em></p>'), `Excerpt not included in '${item.content}'`)
+})

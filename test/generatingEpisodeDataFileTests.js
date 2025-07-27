c7177a4a0a73f6f06de8d7a3c74bd8685be6ad1d
Nathan Bottomley <nathan.bottomley@gmail.com>
2025-07-27 17:18:02 +1000

    Fixed error in test caused by renaming fixture

--- test/generatingEpisodeDataFileTests.js
+++ test/generatingEpisodeDataFileTests.js
@@ -3,14 +3,14 @@ import Eleventy from '@11ty/eleventy'
 import path from 'node:path'
 import { existsSync, readFileSync, unlinkSync } from 'node:fs'
 
-const fixture = './fixtures/generatingEpisodesDataFile'
+const fixture = './fixtures/generatingEpisodeDataFile'
 const episodeDataFilePath = path.join(process.cwd(), `${fixture}/src/_data/episodeData.json`)
 
 test.afterEach('delete episodesData file', async (t) => {
   unlinkSync(episodeDataFilePath)
 })
 
-test.serial('the `episodesData.json` file is generated', async (t) => {
+test.serial('the `episodeData.json` file is generated', async (t) => {
   const eleventy = new Eleventy(
     `${fixture}/src`,
     `${fixture}/_site`,
@@ -21,7 +21,7 @@ test.serial('the `episodesData.json` file is generated', async (t) => {
   t.true(existsSync(episodeDataFilePath))
 })
 
-test.serial('the `episodesData.json` file is correct', async (t) => {
+test.serial('the `episodeData.json` file is correct', async (t) => {
   const eleventy = new Eleventy(
     `${fixture}/src`,
     `${fixture}/_site`,
@@ -29,8 +29,8 @@ test.serial('the `episodesData.json` file is correct', async (t) => {
       configPath: `${fixture}/eleventy.config.js`
     })
   await eleventy.toJSON()
-  const episodesData = JSON.parse(readFileSync(episodeDataFilePath))
-  const expectedEpisodesData = {
+  const episodeData = JSON.parse(readFileSync(episodeDataFilePath))
+  const expectedEpisodeData = {
     '2GAB 1, The Star Beast.mp3': {
       size: 32004399,
       duration: 1947.2979591836734
@@ -40,5 +40,5 @@ test.serial('the `episodesData.json` file is correct', async (t) => {
       duration: 1587.7485714285715
     }
   }
-  t.deepEqual(episodesData, expectedEpisodesData)
+  t.deepEqual(episodeData, expectedEpisodeData)
 })
